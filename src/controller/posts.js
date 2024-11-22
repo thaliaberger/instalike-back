750b196b841937c85cace32bbfa0dc22f14a2a87
Thalía Berger <thaliaberger@hotmail.com>
2024-11-22 16:33:44 -0300

    cria rota para criar novo post

--- src/controller/posts.js
+++ src/controller/posts.js
@@ -1,6 +1,18 @@
-import {getAllPosts} from "../model/posts.js"
+import { getAllPosts, createNewPost } from "../model/posts.js"
 
 export async function listPosts(req, res) {
     const posts = await getAllPosts();
     res.status(200).send(posts);
-}
+};
+
+export async function createPost(req, res) {
+    const content = req.body;
+
+    try {
+        const newPost = await createNewPost(content);
+        res.status(200).send(newPost);
+    } catch(error) {
+        console.error(error.message);
+        res.status(500).json({"Error": "Bad request"});
+    }
+};
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { listPosts } from "../controller/posts.js"
+import { createPost, listPosts } from "../controller/posts.js"
 
 const routes = (app) => {
     app.use(express.json());
 
     app.get("/posts", listPosts);
+
+    app.post("/posts", createPost);
 };
 
 export default routes;
