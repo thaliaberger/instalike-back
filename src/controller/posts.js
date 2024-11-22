1294cd7a9ccecf8721b2d72690f06b273851fb41
Thalía Berger <thaliaberger@hotmail.com>
2024-11-22 17:04:28 -0300

    implementa upload

--- src/controller/posts.js
+++ src/controller/posts.js
@@ -1,4 +1,5 @@
 import { getAllPosts, createNewPost } from "../model/posts.js"
+import fs from "fs";
 
 export async function listPosts(req, res) {
     const posts = await getAllPosts();
@@ -10,6 +11,24 @@ export async function createPost(req, res) {
 
     try {
         const newPost = await createNewPost(content);
+        res.status(200).send(newPost);
+    } catch(error) {
+        console.error(error.message);
+        res.status(500).json({"Error": "Bad request"});
+    }
+};
+
+export async function uploadImage(req, res) {
+    const post = {
+        description: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+
+    try {
+        const newPost = await createNewPost(post);
+        const image = `uploads/${newPost.insertedId}.png`;
+        fs.renameSync(req.file.path, image);
         res.status(200).send(newPost);
     } catch(error) {
         console.error(error.message);
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,12 +1,24 @@
 import express from "express";
-import { createPost, listPosts } from "../controller/posts.js"
+import multer from "multer";
+import { createPost, listPosts, uploadImage } from "../controller/posts.js"
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage});
 
 const routes = (app) => {
     app.use(express.json());
 
     app.get("/posts", listPosts);
-
     app.post("/posts", createPost);
+    app.post("/upload", upload.single("image"), uploadImage);
 };
 
 export default routes;
