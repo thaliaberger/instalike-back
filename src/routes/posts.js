44984bebc783bbd2492067c629b7611bc4c64b8b
Thalía Berger <thaliaberger@hotmail.com>
2024-11-23 22:12:16 -0300

    use cors

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,6 +1,12 @@
 import express from "express";
 import multer from "multer";
-import { createPost, listPosts, updatePost, uploadImage } from "../controller/posts.js"
+import { createPost, listPosts, updatePost, uploadImage } from "../controller/posts.js";
+import cors from "cors";
+
+const corsOptions =  {
+    origin: "http://locahost:8000",
+    optionsSuccessStatus: 200
+};
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -15,6 +21,7 @@ const upload = multer({ dest: "./uploads" , storage});
 
 const routes = (app) => {
     app.use(express.json());
+    app.use(cors(corsOptions));
 
     app.get("/posts", listPosts);
     app.post("/posts", createPost);
